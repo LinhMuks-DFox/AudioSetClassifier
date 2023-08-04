aa0bb772ae4b118e98ce5188ad67853e36c62197
Mux <muxxum65536@gmail.com>
2023-08-04 13:01:03 +0900

    bug fix

--- prototype.py
+++ prototype.py
@@ -13,7 +13,7 @@ from train_config import *
 device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
 # device = torch.device("cpu")
 # torch.set_num_threads(23)
-dataset = AudioSetForResNet18Classifier.AudioSetForResNet18Classifier(DATA_SET_PATH)
+dataset = AudioSetForResNet18Classifier(DATA_SET_PATH)
 dataset = util_data.Subset(dataset, range(5000))
 data_loader = util_data.DataLoader(dataset, shuffle=False, batch_size=100)
 
