16ffc1c733ad97afcdab55c64207036227d2fb89
Mux <muxxum65536@gmail.com>
2023-08-26 11:04:06 +0900

    new hyper parameter

--- hyper_para.py
+++ hyper_para.py
@@ -1,12 +1,11 @@
-import torch.cuda
+import torch
 
 MODEL = "RES18"
-MODEL_SET = {
-    "RES18",
-    "RES50"
-}
 CLASS_CNT = 527  # Audio set contains 527 class labels
 DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
 BATCH_SIZE = 32
 EPOCHS = 100
 LEARNING_RATE = 1e-5
+GAMMA = 0.99
+DATA_SET = "best"
+TRAIN_TEST_VALIDATE_SPLIT = [0.8, 0.1, 0.1]
