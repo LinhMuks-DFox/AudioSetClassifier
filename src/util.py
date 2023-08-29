ea40cca106e93c9953f53e3daa0ac6c858b63ea7
Mux <muxxum65536@gmail.com>
2023-08-29 17:44:52 +0900

    new module to place utility function

---
+++ src/util.py
@@ -0,0 +1,16 @@
+import functools
+import warnings
+
+
+# a decorator to mark a function as untested
+def untested(func):
+    @functools.wraps(func)
+    def new_func(*args, **kwargs):
+        warnings.simplefilter('always', DeprecationWarning)  # turn off filter
+        warnings.warn("Call to untested function {}.".format(func.__name__),
+                      category=DeprecationWarning,
+                      stacklevel=2)
+        warnings.simplefilter('default', DeprecationWarning)  # reset filter
+        return func(*args, **kwargs)
+
+    return new_func
