3e5b20f6fe82ae7691b03f8777236ee593e47d56
Mux <muxxum65536@gmail.com>
2023-09-09 16:44:04 +0900

    new tags

--- src/tags.py
+++ src/tags.py
@@ -21,3 +21,15 @@ def stable_api(func):
     def new_func(*args, **kwargs):
         return func(*args, **kwargs)
 
+
+def unfinished_api(func):
+    @functools.wraps(func)
+    def new_func(*args, **kwargs):
+        warnings.simplefilter('always', DeprecationWarning)  # turn off filter
+        warnings.warn("Call to unfinished api {}.".format(func.__name__),
+                      category=DeprecationWarning,
+                      stacklevel=2)
+        warnings.simplefilter('default', DeprecationWarning)  # reset filter
+        return func(*args, **kwargs)
+
+    return new_func
