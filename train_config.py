03e9ae747ed24566940ee37810e9ee1c9f4b33ef
Mux <muxxum65536@gmail.com>
2023-09-21 11:26:09 +0900

    remove IN_DOCKER_CONTAINER condition

--- train_config.py
+++ train_config.py
@@ -3,15 +3,16 @@ import sys
 import datetime
 
 IN_DOCKER: bool = os.environ.get("IN_DOCKER_CONTAINER", False)
-if "win" in sys.platform or not IN_DOCKER:
+
+if "win" in sys.platform:
     DATA_SET_PATH: str = r"F:\DataSets\Audioset\balanced\segments\AudioSet.json"
 else:
     DATA_SET_PATH: str = r"data/audio_set/AudioSet.json"
-DRY_RUN: bool = False
+DRY_RUN: bool = True
 DRY_RUN_DATE_SET_LENGTH: int = 80
 
-DUMP_PATH = f"pth_bin/{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}/"
+DUMP_PATH = f"./pth_bin/{datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}"
 if not os.path.exists(DUMP_PATH):
-    os.mkdir(DUMP_PATH)
+    os.makedirs(DUMP_PATH)
 else:
     raise RuntimeError("DUMP PATH ALREADY EXISTS")
