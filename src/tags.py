d75c720022aef50b44f04116675588148f57e0d0
Mux <muxxum65536@gmail.com>
2023-09-24 21:17:01 +0900

    Modify error message

--- src/tags.py
+++ src/tags.py
@@ -2,15 +2,23 @@ import functools
 import warnings
 
 
+class UnfinishedAPIWarning(Warning):
+    pass
+
+
+class UntestedAPIWarning(Warning):
+    pass
+
+
 # a decorator to mark a function as untested
 def untested(func):
     @functools.wraps(func)
     def new_func(*args, **kwargs):
-        warnings.simplefilter('always', DeprecationWarning)  # turn off filter
+        warnings.simplefilter('always', UntestedAPIWarning)  # turn off filter
         warnings.warn("Call to untested function {}.".format(func.__name__),
-                      category=DeprecationWarning,
+                      category=UntestedAPIWarning,
                       stacklevel=2)
-        warnings.simplefilter('default', DeprecationWarning)  # reset filter
+        warnings.simplefilter('default', UntestedAPIWarning)  # reset filter
         return func(*args, **kwargs)
 
     return new_func
@@ -20,17 +28,18 @@ def stable_api(func):
     @functools.wraps(func)
     def new_func(*args, **kwargs):
         return func(*args, **kwargs)
+
     return new_func
 
 
 def unfinished_api(func):
     @functools.wraps(func)
     def new_func(*args, **kwargs):
-        warnings.simplefilter('always', DeprecationWarning)  # turn off filter
+        warnings.simplefilter('always', UnfinishedAPIWarning)  # turn off filter
         warnings.warn("Call to unfinished api {}.".format(func.__name__),
-                      category=DeprecationWarning,
+                      category=UnfinishedAPIWarning,
                       stacklevel=2)
-        warnings.simplefilter('default', DeprecationWarning)  # reset filter
+        warnings.simplefilter('default', UnfinishedAPIWarning)  # reset filter
         return func(*args, **kwargs)
 
     return new_func
