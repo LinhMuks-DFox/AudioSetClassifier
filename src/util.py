d8e2776fc97bd25af6415a638205ab7150328149
Mux <muxxum65536@gmail.com>
2023-09-25 13:54:31 +0900

    change class_num to arguments

--- src/util.py
+++ src/util.py
@@ -2,8 +2,8 @@ import torch
 import typing
 
 
-def label_digit2tensor(label: typing.List[int]) -> torch.Tensor:
-    t = torch.zeros(527)
+def label_digit2tensor(label: typing.List[int], class_num=527) -> torch.Tensor:
+    t = torch.zeros(class_num)
     idx = torch.tensor(label)
     t[idx] = 1
     return t
