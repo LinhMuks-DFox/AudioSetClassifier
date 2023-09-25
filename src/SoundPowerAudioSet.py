fb3c44a17bf253a3cb72303b67c618e6ccdee7ea
Mux <muxxum65536@gmail.com>
2023-09-25 09:50:12 +0900

    test, bug fix

--- src/SoundPowerAudioSet.py
+++ src/SoundPowerAudioSet.py
@@ -9,25 +9,27 @@ import torchaudio.transforms as tch_audio_trans
 from .util import label_digit2tensor
 
 
-@tags.untested
+@tags.stable_api
 class SoundPowerAudioSet(tch_data.Dataset):
     def __init__(self, path: str,
                  sound_track: str,
                  orig_freq: int,
                  new_freq: int,
-                 sample_seconds: int = 10
                  ):
         self.audio_fetcher_ = JsonBasedAudioSet(path)
         self.track_selector_ = sc_transforms.SoundTrackSelector(sound_track)
         self.resampler_ = tch_audio_trans.Resample(orig_freq=orig_freq, new_freq=new_freq)
         self.reshape_size_ = (800, 200)
 
+    @tags.stable_api
     def __len__(self):
         return len(self.audio_fetcher_)
 
-    @tags.untested
+    @tags.stable_api
     def __getitem__(self, index):
         sample, sample_rate, onto, label_digits, label_display = self.audio_fetcher_[index]
+        sample = self.track_selector_(sample)
+        sample = self.resampler_(sample)
         # split sample to 800 chunks
         reshaped_sample = sample.reshape(*self.reshape_size_)  # 16000Hz * 10s // 800 = 200
         sound_power = torch.sum(reshaped_sample ** 2, dim=1) \
---
+++ test/TestSoundPowerAudioSet.py
@@ -0,0 +1,26 @@
+import unittest
+import train_config
+from src.SoundPowerAudioSet import SoundPowerAudioSet
+import hyper_para
+
+
+class MyTestCase(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.test_build_up()
+
+    def test_build_up(self):
+        self.dataset = SoundPowerAudioSet(
+            path=train_config.DATA_SET_PATH,
+            sound_track=hyper_para.AUDIO_PRE_TRANSFORM.get("sound_track"),
+            orig_freq=hyper_para.AUDIO_PRE_TRANSFORM.get("resample").get("orig_freq"),
+            new_freq=hyper_para.AUDIO_PRE_TRANSFORM.get("resample").get("new_freq"),
+        )
+
+    def test_getitem(self):
+        sample, label = self.dataset[0]
+        print(sample.shape)
+
+
+if __name__ == '__main__':
+    unittest.main()
