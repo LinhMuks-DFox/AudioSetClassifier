9b39479e61f918d943ae54e1f18521ad005b8977
Mux <muxxum65536@gmail.com>
2023-10-07 14:16:00 +0900

    add device support

--- src/AutoEncodedAudioSet.py
+++ src/AutoEncodedAudioSet.py
@@ -26,9 +26,10 @@ class AutoEncodedAudioSet(torch.utils.data.Dataset):
                  normalized: bool,
                  sample_seconds: int = 10,
                  output_size: tuple = (10, 80),
-                 encoder_device: torch.device = torch.device('cpu'),
+                 transform_device: torch.device = torch.device('cpu'),
                  ):
         # region data fetch-transform
+        self.transform_device_ = transform_device
         self.audio_fetcher_ = JsonBasedAudioSet(path)
         self.track_selector_ = sc_transforms.SoundTrackSelector(sound_track)
         self.resampler_ = tch_audio_trans.Resample(orig_freq=orig_freq, new_freq=new_freq)
@@ -37,6 +38,11 @@ class AutoEncodedAudioSet(torch.utils.data.Dataset):
                                                                   win_length=win_length,
                                                                   normalized=normalized)
         self.amplitude_trans_ = tch_audio_trans.AmplitudeToDB()
+
+        self.resampler_ = self.resampler_.to(self.transform_device_)
+        self.amplitude_trans_ = self.amplitude_trans_.to(self.transform_device_)
+        self.spectrogram_converter_ = self.spectrogram_converter_.to(self.transform_device_)
+
         # endregion
 
         # region properties
@@ -51,16 +57,15 @@ class AutoEncodedAudioSet(torch.utils.data.Dataset):
         self._split_i = self.sample_seconds_ * self.new_freq_ // 2
         self.output_size_ = output_size
         self.sample_length_ = self.sample_seconds_ * self.new_freq_
-        self.device_ = encoder_device
         # endregion
 
         # region load encoder
         self.auto_encoder: AudioEncoder = make_auto_encoder_from_hyperparameter(self._data_shape_(),
                                                                                 auto_encoder_hypers)[0]
         self.auto_encoder.load_state_dict(
-            torch.load(encoder_model_path, map_location=self.device_)
+            torch.load(encoder_model_path, map_location=self.transform_device_)
         )
-        self.auto_encoder.to(self.device_)
+        self.auto_encoder.to(self.transform_device_)
         # endregion
 
     @tags.stable_api
@@ -68,11 +73,12 @@ class AutoEncodedAudioSet(torch.utils.data.Dataset):
         return len(self.audio_fetcher_)
 
     def __str__(self):
-        return f"AutoEncodedAudioSet: length({len(self)}), using device: {self.device_}\n"
+        return f"AutoEncodedAudioSet: length({len(self)}), using device: {self.transform_device_}\n"
 
     @tags.stable_api
     def _data_shape_(self):
-        sample = self.audio_fetcher_[0][0]
+        sample: torch.Tensor = self.audio_fetcher_[0][0]
+        sample = sample.to(self.transform_device_)
         sample = self.track_selector_(sample)
         sample = self.resampler_(sample)
         sample = fix_length(sample, self.sample_length_)
@@ -85,6 +91,7 @@ class AutoEncodedAudioSet(torch.utils.data.Dataset):
     def __getitem__(self, index: int):
         sample, sample_rate, onto, label_digits, label_display = self.audio_fetcher_[index]
         label = label_digit2tensor(label_digits)
+        sample: torch.Tensor = sample.to(self.transform_device_)
         track = self.track_selector_(sample)
         track = self.resampler_(track)
         track = fix_length(track, self.sample_length_)
@@ -95,10 +102,10 @@ class AutoEncodedAudioSet(torch.utils.data.Dataset):
 
         prev5s_spe_db = self.amplitude_trans_(prev5s_spe)
         post5s_spe_db = self.amplitude_trans_(post5s_spe)
-        prev5s_spe_db, post5s_spe_db = prev5s_spe_db.to(self.device_), post5s_spe_db.to(self.device_)
+        prev5s_spe_db, post5s_spe_db = prev5s_spe_db.to(self.transform_device_), post5s_spe_db.to(
+            self.transform_device_)
         pres5s_auto_encoded = self.auto_encoder(prev5s_spe_db)
         posts5s_auto_encoded = self.auto_encoder(post5s_spe_db)
 
         x = torch.hstack([pres5s_auto_encoded, posts5s_auto_encoded])
-        # x.reshape(80, 10)
         return x.reshape(*self.output_size_), label
--- test/TestAutoEncodedAudioSet.py
+++ test/TestAutoEncodedAudioSet.py
@@ -1,7 +1,9 @@
 import unittest
 
+import torch
 import hyper_para
 import train_config
+import train_prepare
 from src.AutoEncodedAudioSet import AutoEncodedAudioSet
 
 
@@ -10,12 +12,14 @@ class MyTestCase(unittest.TestCase):
     def setUp(self) -> None:
         self.dataset = None
         self.test_build_up()
+        self.data_on_device = (torch.empty(0)
+                               .to(train_prepare.select_device(hyper_para.DATA_TRANSFORM_DEVICE)))
 
     def test_build_up(self) -> None:
         self.dataset = AutoEncodedAudioSet(
             auto_encoder_hypers=hyper_para.AUTO_ENCODER_MODEL,
             encoder_model_path=r"../pre_trained_encoder/2023-08-04-09-49-03/encoder.pth",
-            path=train_config.DATA_SET_PATH,
+            path=r"../data/audio_set/AudioSet.json",
             sound_track=hyper_para.AUDIO_PRE_TRANSFORM.get("sound_track"),
             orig_freq=hyper_para.AUDIO_PRE_TRANSFORM.get("resample").get("orig_freq"),
             new_freq=hyper_para.AUDIO_PRE_TRANSFORM.get("resample").get("new_freq"),
@@ -24,12 +28,18 @@ class MyTestCase(unittest.TestCase):
             win_length=hyper_para.AUDIO_PRE_TRANSFORM.get("fft").get("win_length"),
             normalized=hyper_para.AUDIO_PRE_TRANSFORM.get("fft").get("normalized"),
             output_size=hyper_para.ENCODED_AND_SOUND_POWER_DATASET_RESHAPE_SIZE,
-            encoder_device=hyper_para.DATA_TRANSFORM_DEVICE,
+            transform_device=hyper_para.DATA_TRANSFORM_DEVICE,
         )
 
     def test_getitem(self):
-        sample, label = self.dataset[0]
-        print(sample.shape)
+        self.sample0, self.label0 = self.dataset[0]
+        print(self.sample0.shape)
+
+    def test_device(self):
+        self.test_getitem()
+        print(self.sample0.device)
+        print(self.data_on_device.device)
+        self.assertTrue(self.sample0.device == self.data_on_device.device)
 
 
 if __name__ == '__main__':
