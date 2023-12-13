cf23914796eca9273505150f4829e89a83dbe246
mux <muxxum65536@gmail.com>
2023-12-13 17:40:33 +0900

    rename dump path

--- train_config.py
+++ train_config.py
@@ -25,10 +25,10 @@ else:
     TRAIN_DATA_SET_JSON: str = r"./subset_json/music_speech/sub_train.json"
 
 PLATFORM: str = plf
-DRY_RUN: bool = False
+DRY_RUN: bool = True
 CPU_N_WORKERS: int = 23
 TRAIN_ID = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
-DUMP_PATH = f"./pth_bin/2023-10-28-ablation/{TRAIN_ID}"
+DUMP_PATH = f"./pth_bin/latent600-ablation/{TRAIN_ID}"
 CLASS_LABELS_INDICES = r"subset_json/music_speech/sub_set_cls_label_idx.json"
 AUTO_ENCODER_MODEL_PATH = r"pre_trained_encoder/2023-7-27-ablation/normal/encoder.pth"
 TRAIN_CONFIG_SUMMARY = f"""
