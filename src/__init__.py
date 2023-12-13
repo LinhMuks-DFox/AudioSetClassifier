65545eac5cf1995840ef3a54341fb9f28931806a
mux <muxxum65536@gmail.com>
2023-12-13 13:48:52 +0900

    reformat

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,8 @@
 from .AutoEncodedAudioSet import AutoEncodedAudioSet
 from .FullSpectroAudioSet import FullSpectroAudioSet
-from .SoundPowerAudioSet import SoundPowerAudioSet
 from .MultiLabelClassifierTester import MultiLabelClassifierTester
+from .SoundPowerAudioSet import SoundPowerAudioSet
+
 __all__ = [
     "FullSpectroAudioSet",
     "SoundPowerAudioSet",
